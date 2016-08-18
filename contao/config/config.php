5c4d24683b07a932f973f2b111192493ef752917
Sven Baumann <baumann.sv@gmail.com>
2016-08-18 13:18:21 +0200

    create it

---
+++ contao/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Form field continuously number for Contao OpenSource CMS
+ * Copyright © 2016 ContaoBlackForest
+ *
+ * PHP version 5
+ *
+ * @copyright  ContaoBlackForest 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @package    contaoblackforest/contao-form-continuously-number
+ * @license    LGPL-3.0+
+ */
---
+++ src/Widget.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Form field continuously number for Contao OpenSource CMS
+ * Copyright © 2016 ContaoBlackForest
+ *
+ * PHP version 5
+ *
+ * @copyright  ContaoBlackForest 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @package    contaoblackforest/contao-form-continuously-number
+ * @license    LGPL-3.0+
+ */
+
+namespace ContaoBlackForest\Contao\Form\ContinuouslyNumbers;
+
+/**
+ * This class make a form field for continuously number.
+ *
+ * @package ContaoBlackForest\Contao\Form\ContinuouslyNumbers
+ */
+class Widget
+{
+
+}
